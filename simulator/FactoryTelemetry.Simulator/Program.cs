06f14cf5abf93d1437b45bd8395dd6279b38b8ad
Ricardo Rocheta <ricardo.rocheta@cheto.eu>
2026-01-21 12:07:28 +0000

    feat(simulator): initial project setup

---
+++ simulator/FactoryTelemetry.Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryTelemetry.Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
